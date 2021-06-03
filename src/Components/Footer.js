91e920ea5e619367aa3e324bb7b4d6c4112801ee
Chris Hendrix <chendrix1123@gmail.com>
2021-06-03 18:30:31 +0000

    github pages attempt 1

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2021 Chris Hendrix</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a> and <a title="TimBaker" href="https://github.com/tbakerx/react-resume-template">Tim Baker</a></li>
            </ul>
 
         </div>
