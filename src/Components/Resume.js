dfd65a1c1720b57e90f98f6df7444df1f2611d65
Chris Hendrix <chendrix1123@gmail.com>
2021-06-10 20:56:38 +0000

    css and layout updates

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,6 +5,10 @@ class Resume extends Component {
 
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
+      var skills = this.props.data.skills.map(function(skills){
+        var className = 'bar-expand '+skills.name.toLowerCase();
+        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+      })
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
@@ -16,14 +20,30 @@ class Resume extends Component {
             <p>{work.description}</p>
         </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+      
     }
 
     return (
       <section id="resume">
+      <div className="row skill">
+
+         <div className="three columns header-col">
+            <h1><span>Skills</span></h1>
+         </div>
+
+         <div className="nine columns main-col">
+
+            <p>{skillmessage}
+            </p>
+
+				<div className="bars">
+				   <ul className="skills">
+					  {skills}
+					</ul>
+				</div>
+			</div>
+      </div>
+      
       <div className="row work">
 
          <div className="three columns header-col">
@@ -53,24 +73,7 @@ class Resume extends Component {
 
 
 
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
-      </div>
+      
    </section>
     );
   }
