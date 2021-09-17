e77b6b0d634aa25423d036a46c2f77598a378fd6
Chris Hendrix <chendrix1123@gmail.com>
2021-09-17 13:05:20 -0400

    added bullet functionality and edited data json

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,82 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Resume extends Component {
-  render() {
+	render() {
+		if (this.props.data) {
+			var skillmessage = this.props.data.skillmessage;
+			var skills = this.props.data.skills.map(function (skills) {
+				var className = 'bar-expand ' + skills.name.toLowerCase();
+				return (
+					<li key={skills.name}>
+						<span style={{ width: skills.level }} className={className}></span>
+						<em>{skills.name}</em>
+					</li>
+				);
+			});
+			var education = this.props.data.education.map(function (education) {
+				return (
+					<div key={education.school}>
+						<h3>{education.school}</h3>
+						<p className='info'>
+							{education.degree} <span>&bull;</span>
+							<em className='date'>{education.graduated}</em>
+						</p>
+						<p>{education.description}</p>
+					</div>
+				);
+			});
+			var work = this.props.data.work.map(function (work) {
+				return (
+					<div key={work.company}>
+						<h3>{work.company}</h3>
+						<p className='info'>
+							{work.title}
+							<span>&bull;</span> <em className='date'>{work.years}</em>
+						</p>
+						{work.description && work.description != '' && <p>{work.description}</p>}
+						{work.bullets && work.bullets.length > 0 && (
+							<ul className='circle'>
+								{work.bullets.map((bullet) => (
+									<li style={{ lineHeight: '2' }}>{bullet}</li>
+								))}
+							</ul>
+						)}
+					</div>
+				);
+			});
+		}
 
-    if(this.props.data){
-      var skillmessage = this.props.data.skillmessage;
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
-        </div>
-      })
-      
-    }
+		return (
+			<section id='resume'>
+				<div className='row skill'>
+					<div className='three columns header-col'>
+						<h1>
+							<span>Skills</span>
+						</h1>
+					</div>
 
-    return (
-      <section id="resume">
-      <div className="row skill">
+					<div className='nine columns main-col'>
+						<p>{skillmessage}</p>
 
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
+						<div className='bars'>
+							<ul className='skills'>{skills}</ul>
+						</div>
+					</div>
 				</div>
-			</div>
-      </div>
-      
-      <div className="row work">
 
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
+				<div className='row work'>
+					<div className='three columns header-col'>
+						<h1>
+							<span>Work</span>
+						</h1>
+					</div>
 
-         <div className="nine columns main-col">
-          {work}
-        </div>
-      </div>
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
+					<div className='nine columns main-col'>{work}</div>
+				</div>
+				<div className='row education'>
+					<div className='three columns header-col'>
+						<h1>
+							<span>Education</span>
+						</h1>
+					</div>
 
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
-      </div>
-
-
-     
-
-
-
-      
-   </section>
-    );
-  }
+					<div className='nine columns main-col'>
+						<div className='row item'>
+							<div className='twelve columns'>{education}</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
+	}
 }
 
 export default Resume;
